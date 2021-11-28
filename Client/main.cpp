2684b5276e6c28044074e8e05caddda830b17d7d
LEE-HYUN-JI <brightwisdom226@gmail.com>
2021-11-28 22:27:55 +0900

    add notes

--- Client/main.cpp
+++ Client/main.cpp
@@ -7,22 +7,22 @@ using namespace std;
 
 int main() {
 	try {
-		boost::asio::io_service io_service;
-		tcp::resolver resolver(io_service);
-		tcp::resolver::query query("localhost", "daytime");
-		tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+		boost::asio::io_service io_service;//(0)통신을 위한 IO Service객체 생성
+		tcp::resolver resolver(io_service);//(1)resolver를 통해 도메인 이름을 TCP종단점으로 변환
+		tcp::resolver::query query("localhost", "daytime");//(2)DNS서버에 질의문을 보냄(localhost(IP주소)에 daytime프로토콜(13번 포트)로 접속하겠다는 준비)
+		tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);//(3)endpoint객체로 서버와의 연결을 수행
 		tcp::socket socket(io_service);
-		boost::asio::connect(socket, endpoint_iterator);
+		boost::asio::connect(socket, endpoint_iterator);//(4)하나의 소캣 객체를 생성하여, 해당 소켓으로 서버와 연결
 
 		while (1) {
 			boost::array<char, 128> buf;
-			boost::system::error_code error;
-			size_t len = socket.read_some(boost::asio::buffer(buf), error);
+			boost::system::error_code error;//버퍼 및 오류 처리 변수 선언
+			size_t len = socket.read_some(boost::asio::buffer(buf), error);//(5)버퍼를 이용하여 서버로부터 데이터를 받아옴(read_some)
 			if (error == boost::asio::error::eof)
 				break;
 			else if (error)
 				throw boost::system::system_error(error);
-			cout.write(buf.data(), len);
+			cout.write(buf.data(), len);//(6)버퍼에 담긴 데이터를 출력
 		}
 	}
 	catch (exception& e) {
--- Server/main.cpp
+++ Server/main.cpp
@@ -8,22 +8,22 @@ using boost::asio::ip::tcp;
 using namespace std;
 
 string make_daytime_string() {
-	time_t now = time(0);
-	return ctime(&now);
+	time_t now = time(0);//시간정보 초기화('time_t'는 time()함수의 반환형)
+	return ctime(&now);//문자열 형태로 서버 컴퓨터의 날짜 및 시간정보 반환
 }
 
 int main()
 {
 	try {
-		boost::asio::io_service io_service;
-		tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 13));
+		boost::asio::io_service io_service;//(0)IO Service 객체 생성
+		tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 13));//(1)13번 포트로 연결받는 수동 포켓 생성
 		while (1)
 		{
 			tcp::socket socket(io_service);
-			acceptor.accept(socket);
-			string message = make_daytime_string();
+			acceptor.accept(socket);//(2)클라이언트 소켓 생성 후, 연결을 기다림
+			string message = make_daytime_string();//make_daytime_string()함수를 이용해 보낼 메시지 생성
 			boost::system::error_code ignored_error;
-			boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+			boost::asio::write(socket, boost::asio::buffer(message), ignored_error);//(3)메시지를 버퍼에 담아 전송
 		}
 	}
 	catch (exception& e) {
